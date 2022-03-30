63ded1b671b70e49dfb1fb3086cb5e61ce156622
Cletip <clement020302@gmail.com>
2022-03-30 18:28:32 +0200

    début + dep de xah fly keys

---
+++ LayerOfXahFlyKey.el
@@ -0,0 +1,23 @@
+;;; LayerOfXahFlyKey.el --- a simple package                     -*- lexical-binding: t; -*-
+
+;; Copyright © 2021, by Cletip
+
+;; Author: Cletip
+;; Version: 0.1
+;; Created: 30 Mars 2022
+;; Package-Requires: ((undo-tree "0.6.5"))
+;; Keywords: 
+;; License: GPL v3
+
+;; This file is not part of GNU Emacs.
+
+    ;;; Commentary:
+
+;;; Code:
+
+(tool-bar-mode 1)
+
+
+
+(provide 'LayerOfXahFlyKey)
+;;; LayerOfXahFlyKey.el ends here
